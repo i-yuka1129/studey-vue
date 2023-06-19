45c71a065aeea4314de1bb02e851a1dbeb79e96f
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-19 13:05:23 +0900

    フォーム入力との同期まで

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el:'#app',
+  data: {
+    message:'初期メッセージ',
+    list: ['りんご', 'ばなな', 'いちご', 'もも'],
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target)
+    }
+  }
+})
