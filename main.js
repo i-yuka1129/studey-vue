4fad37b84634f1950ce605e4763f20fa915d2d1d
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-21 10:23:36 +0900

    SVGのデータバインディング

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    item: {
-      id: 1,
-      class: 'item01',
-      width: 200,
-      height: 200,
-      style: 'background-color: lightblue;',
-    }
+    radius: 10
   }
 })
