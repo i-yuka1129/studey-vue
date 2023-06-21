79b8d7cebfb940298a74adffb784ec96948f9316
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-21 12:01:52 +0900

    リストの更新

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
+    name: 'キマイラ',
     list: [
       { id: 1, name: 'スライム', hp: 100},
       { id: 2, name: 'ゴブリン', hp: 200},
       { id: 3, name: 'ドラゴン', hp: 500},
     ]
+  },
+  methods: {
+    doAdd: function() {
+      var max = this.list.reduce(function (a, b) {
+        return a > b.id ? a: b.id
+      }, 0)
+      this.list.push({
+        id:max + 1,
+        name: this.name,
+        hp: 500
+      })
+    }
   }
 })
