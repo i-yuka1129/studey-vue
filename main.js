f9ad88dd2c06d5a110d435837b8f2bcd1faa899a
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-23 10:29:41 +0900

    リスト要素の置き換え プロパティの追加

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    list: []
+    list: [
+      { id: 1, name: 'スライム', hp: 100},
+      { id: 2, name: 'ゴブリン', hp: 200},
+      { id: 3, name: 'ドラゴン', hp: 500},
+    ]
   },
   created: function() {
-    axios.get('list.json').then(function (response) {
-      this.list = response.data
-    }.bind(this)).catch(function (e) {
-      console.error(e)
-    })
+    this.list.forEach(function(item) {
+      this.$set(item, 'active', false)
+    }, this)
+    this.$set(this.list, 1, { id: 1, name: 'キングスライム', hp: 500})
   }
 })
