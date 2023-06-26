fb779a3496ba0e929c2ea62f494e5117ea2f17d7
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-26 11:18:57 +0900

    その他の入力タイプ

--- main.js
+++ main.js
@@ -2,14 +2,6 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    preview: ''
-  },
-  methods: {
-    handleChange: function(event) {
-      var file = event.target.files[0]
-      if (file && file.type.match(/^image\/(png|jpeg)$/)) {
-        this.preview = window.URL.createObjectURL(file)
-      }
-    }
+    price: 100
   }
 })
