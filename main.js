deece397041699a81cc5162b523102d0c5359da1
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-26 10:35:28 +0900

    v-modelの使い方

--- main.js
+++ main.js
@@ -2,11 +2,6 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    message: 'Hello Vue,js',
-  },
-  methods: {
-    doDelete: function() {
-      console.log('削除')
-    }
+    message: '日本語'
   }
 })
