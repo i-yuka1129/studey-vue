133f5f65ca971c46d7cf8a91e933ebead96317d9
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-26 11:28:54 +0900

    スクロールイベントの取得

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    price: 100
+    scrollY: 0,
+    timer: null
+  },
+  created: function() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy: function() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll:function() {
+      if (this.timer === null) {
+        this.timer = setTimeout(function () {
+          this.scrollY = window.scrollY
+          clearTimeout(this.timer)
+          this.timer = null
+        }.bind(this), 200)
+      }
+    }
   }
 })
