1de743aa701f184cb6794b5f483da7afef1e4b39
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-27 10:39:20 +0900

    算出プロパティを組み合わせ使用する

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    width: 800
+    width: 800,
+    height: 600
   },
   computed: {
     halfWidth: function() {
       return this.width / 2
+    },
+    halfHight: function() {
+      return this.height / 2
+    },
+    halfPoint: function() {
+      return {
+        x: this.halfWidth,
+        y: this.halfHight
+      }
     }
   }
 })
