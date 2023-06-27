2c54965304f6d11959c6060b28a1cb9fb41bedab
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-27 11:18:17 +0900

    リストの絞り込み

--- main.js
+++ main.js
@@ -2,16 +2,27 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    width: 800
+    // フォームの入力と紐づけるデータ
+    budget: 300,
+    // 表示件数
+    limit: 2,
+    // もとになるリスト
+    list: [
+      {id: 1, name: 'りんご', price: 100},
+      {id: 2, name: 'ばなな', price: 200},
+      {id: 3, name: 'いちご', price: 300},
+      {id: 4, name: 'めろん', price: 400},
+      {id: 5, name: 'すいか', price: 500},
+    ]
   },
   computed: {
-    computedData: function() {
-      return Math.random() 
-    }
-  },
-  methods: {
-    methodsData: function() {
-      return Math.random() 
+    matched: function() {
+      return this.list.filter(function(el) {
+        return el.price <= this.budget
+      }, this)
+    },
+    limited: function() {
+      return this.matched.slice(0, this.limit)
     }
   }
 })
