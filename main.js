5a52511f61070c9f316bc1019bec35f3c9ce620e
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-27 10:52:31 +0900

    ゲッターとセッター

--- main.js
+++ main.js
@@ -2,20 +2,15 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    width: 800,
-    height: 600
+    width: 800
   },
   computed: {
-    halfWidth: function() {
-      return this.width / 2
-    },
-    halfHight: function() {
-      return this.height / 2
-    },
-    halfPoint: function() {
-      return {
-        x: this.halfWidth,
-        y: this.halfHight
+    halfWidth: {
+      get: function() {
+        return this.width / 2
+      },
+      set: function(value) {
+        this.width = value * 2
       }
     }
   }
