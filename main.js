4ec436c758397be6c354f658dc7e27cc4b60f8b7
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-27 17:51:00 +0900

    フィルタの使い方

--- main.js
+++ main.js
@@ -2,22 +2,11 @@ Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
   data: {
-    list: [],
-    current: '',
-    topics: [
-      { value: 'vue', name: 'Vue.js'},
-      { value: 'jQuery', name: 'jQuery'},
-    ]
+    price: 19800
   },
-  watch: {
-    current: function(val) {
-      axios.get('https://api.github.com/search/repositories', {
-        params: {
-          q: 'topic' + val
-        }
-      }).then(function (response) {
-        this.list = response.data.items
-      }.bind(this))
+  filters: {
+    localeNum: function (val) {
+      return val.toLocaleString()
     }
   }
 })
