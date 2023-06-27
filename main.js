9e737debb4449a229f2d6768c0b948380004e1a8
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-27 13:27:08 +0900

    unwatchを利用する

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
-  watch: {
-    this.$watch('value', function() {
-      
+  data: {
+    edited: false,
+    list: [
+      { id: 1, name: 'りんご', price: 100},
+      { id: 2, name: 'ばなな', price: 200},
+    ]
+  },
+  created: function() {
+    var unwatch = this.$watch('list', function() {
+      this.edited = true
+      unwatch()
+    }, {
+      deep: true
     })
   }
 })
