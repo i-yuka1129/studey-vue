d8b4b27383e2e4a906e404aefb07dbfcb3b8a874
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-28 10:51:43 +0900

    更新後のDOMの高さを取得する

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 Vue.config.devtools = true;
 var app = new Vue({
   el:'#app',
-})
-
-Vue.directive('example', {
-  bind: function (el, binding) {
-    console.log('v-example bind')
+  data: {
+    list: []
   },
-  inserted: function (el, binding) {
-    console.log('v-example inserted')
-  },
-  update: function (el, binding) {
-    console.log('v-example update')
-  },
-  componentUpdated: function (el, binding) {
-    console.log('v-example componentUpdated')
-  },
-  unbind: function (el, binding) {
-    console.log('v-example unbind')
+  watch: {
+    list: function() {
+      console.log('通常', this.$refs.list.offsetHeight)
+      this.$nextTick(function() {
+        console.log('nextTick', this.$refs.list.offsetHeight)
+      })
+    }
   }
 })
