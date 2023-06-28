3af0fbe0e2db25f61682c9ffb0701855290b431f
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-28 18:10:15 +0900

    子コンポーネントを参照する$refs

--- main.js
+++ main.js
@@ -1,50 +1,19 @@
 Vue.config.devtools = true;
 
-var bus = new Vue({
-  data: {
-    count: 0
-  }
-})
-
-Vue.component('component-b', {
-  template: '<p>{{ bus.count }}</p>',
-  computed: {
-    bus: function () {
-      return bus.$data
-    }
-  },
+Vue.component('comp-child', {
+  template: '<div>...</div>',
   created: function () {
-    bus.$on('bus-event', function () {
-      this.count++
+    this.$on('open', function () {
+      console.log('何か処理')
     })
-  },
-  props: {
-    id: Number,
-    name: String,
-    hp: Number
-  },
-  methods: {
-    doAttack: function () {
-      this.$emit('attack', this.id)
-    }
   }
 })
 
 new Vue({
   el:'#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100},
-      { id: 2, name: 'ゴブリン', hp: 200},
-      { id: 3, name: 'ドラゴン', hp: 500},
-    ]
-  },
   methods: {
-    handleAttack: function (id) {
-      var item = this.list.find(function (el) {
-        return el.id === id
-      })
-      if (item !== undefined && item.hp > 0)item.hp -= 10
+    handleClick: function () {
+      this.$refs.child.$emit('open')
     }
   }
 })
