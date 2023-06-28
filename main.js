9bc3ddaaaa8bd4f7676693144aa3d4920fcd7ce5
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-06-28 18:50:25 +0900

    スロットを使ったカスタマイズ

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
 Vue.config.devtools = true;
 
-Vue.component('comp-child', {
-  template: '<div>...</div>',
-  created: function () {
-    this.$on('open', function () {
-      console.log('何か処理')
-    })
-  }
-})
-
 new Vue({
   el:'#app',
-  methods: {
-    handleClick: function () {
-      this.$refs.child.$emit('open')
-    }
-  }
 })
