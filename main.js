db3d12cace334ceb8b8a3d5ad44541e664978ac3
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-07-05 12:31:53 +0900

    keep-alive で状態を維持する

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 Vue.config.devtools = true;
 
-var mixin = {
-  created: function () {
-    this.hello()
-  },
-  methods: {
-    hello: function () {
-      console.log('hello from mixin!')
+// メッセージ一覧用コンポーネント
+Vue.component('comp-board', {
+  template: '<div>Message Board</div>',
+})
+// 入力フォーム用コンポーネント
+Vue.component('comp-form', {
+  template: '<div>Form<textarea v-model="message"></textarea></div>',
+  data: function () {
+    return {
+      message: ''
     }
   }
-}
-
-Vue.component('my-component-a', {
-  mixin: [mixin],
-  template: '<p>MyComponentA</p>'
-})
-Vue.component('my-component-b', {
-  mixin: [mixin],
-  template: '<p>MyComponentB</p>'
 })
 
 new Vue({
   el: '#app',
   data: {
-    componentTypes: ['my-component-a', 'my-component-b'],
-    current: 0
-  },
-  computed: {
-    component: function () {
-      return this.componentTypes[this.current]
-    }
+    current: 'comp-board' // 動的に切り替える
   }
 })
