b895039843655569a58c5c85a76668569cf95313
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-07-05 12:08:34 +0900

    テンプレートの種類

--- main.js
+++ main.js
@@ -1,40 +1,9 @@
 Vue.config.devtools = true;
 
 Vue.component('my-component', {
-  template: '<div class="my-component">\
-  <p>名前.{{ name }} HP.{{ hp }}</p>\
-  <p>名前 <input v-model="localName"></p>\
-  <p>HP <input size="5" v-model.number="localHp"></p>\
-  </div>',
-  props: {
-    name: String,
-    hp: Number
-  },
-  computed: {
-    // 算出プロパティのセッター＆ゲッターを使ってv-modelを使用
-    localName: {
-      get: function () {
-        return this.name
-      },
-      set: function (val) {
-        this.$emit('update:name', val)
-      }
-    },
-    localHp: {
-      get: function () {
-        return this.hp
-      },
-      set: function (val) {
-        this.$emit('update:hp', val)
-      }
-    }
-  }
+  template: '#child-template'
 })
 
 new Vue({
   el: '#app',
-  data: {
-    name: 'スライム',
-    hp: 100
-  }
 })
