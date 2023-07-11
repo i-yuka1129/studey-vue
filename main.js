0ba71f955c97b6de65577ea141ff931e2f483a6d
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-07-11 11:55:24 +0900

    基本的なトランジションの使い方

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
 Vue.config.devtools = true;
 
-// メッセージ一覧用コンポーネント
-Vue.component('comp-board', {
-  template: '<div>Message Board</div>',
-})
-// 入力フォーム用コンポーネント
-Vue.component('comp-form', {
-  template: '<div>Form<textarea v-model="message"></textarea></div>',
-  data: function () {
-    return {
-      message: ''
-    }
-  }
-})
-
 new Vue({
   el: '#app',
   data: {
-    current: 'comp-board' // 動的に切り替える
+    show: true
   }
 })
