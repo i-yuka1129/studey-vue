79603ef8da855991f17eec0d39926b3f2c5783fa
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-07-20 17:40:20 +0900

    SVGのトランジション

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 Vue.config.devtools = true;
 
+Vue.component('my-circle', {
+  template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+  props: {
+    fill: String
+  }
+})
+
 new Vue({
   el: '#app',
   data: {
-    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    toggle: false
   },
-  methods: {
-    doShuffle: function() {
-      this.list = _.shuffle(this.list)
-    },
-    doAdd: function() {
-      var newNumber = Math.max.apply(null, this.list) + 1
-      var index = Math.floor(Math.random() * (this.list.length + 1 ))
-      this.list.splice(index, 0, newNumber)
-    },
-    doRemove: function() {
-      this.list.splice(index, 1)
+  computed: {
+    fill: function() {
+      return this.toggle ? 'lightpink' : 'skyblue'
     }
   }
 })
