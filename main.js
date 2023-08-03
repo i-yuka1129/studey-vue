4bc81a79bc1ae70e9536258832444c6e517b02cb
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-08-03 17:09:28 +0900

    ES2015 で書いてみよう関数とメソッドの書き方

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 Vue.config.devtools = true;
 
-var windowPlugin = {
-  install: function(Vue) {
-    // プラグインデータ用にVueインスタンスを利用する
-    var store = new Vue({
-      data: {
-        scrollY: 0
-      }
-    })
-    // ウィンドウのスクロールイベントをハンドル
-    var timer = null
-    window.addEventListener('scroll', function() {
-      if(timer === null) {
-        timer = this.setTimeout(function() {
-          // 200ms間隔でscrollYプロパティに代入
-          clearTimeout(timer)
-          timer = null
-        }, 200)
-      }
-    })
-    // インスタンスプロパティに登録
-    Vue.prototype.$window = store.$data
-  }
-}
-
-Vue.use(windowPlugin)
-
-Vue.component('my-component', {
-  template: '<div>{{ scrollY }}</div>',
-  computed: {
-    scrollY: function() {
-      return this.$window.scrollY
-    }
+new Vue({
+  methods: {
+    handleClick() { ... }
   }
 })
