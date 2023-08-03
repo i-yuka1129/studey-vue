3d0d1a1355effa482b0d44743a8e73a922b0c882
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-08-03 16:59:10 +0900

    Vue.jsプラグイン

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 Vue.config.devtools = true;
 
-import Example from 'Example.js'
+var windowPlugin = {
+  install: function(Vue) {
+    // プラグインデータ用にVueインスタンスを利用する
+    var store = new Vue({
+      data: {
+        scrollY: 0
+      }
+    })
+    // ウィンドウのスクロールイベントをハンドル
+    var timer = null
+    window.addEventListener('scroll', function() {
+      if(timer === null) {
+        timer = this.setTimeout(function() {
+          // 200ms間隔でscrollYプロパティに代入
+          clearTimeout(timer)
+          timer = null
+        }, 200)
+      }
+    })
+    // インスタンスプロパティに登録
+    Vue.prototype.$window = store.$data
+  }
+}
 
-console.log(Example.count)
+Vue.use(windowPlugin)
 
-new Vue({
-  el: '#app',
-  data: {
-    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  },
-  methods: {
-    doShuffle: function () {
-      this.list = _.shuffle(this.list)
-    },
-    doAdd: function() {
-      var newNumber = Math.max.apply(null, this.list) + 1
-      var index = Math.floor(Math.random() * (this.list.length + 1))
-      this.list.splice(index, 0, newNumber)
-    },
-    doRemove: function (index) {
-      this.list.splice(index, 1)
-    },
-    beforeLeave: function(el) {
-      var style = window.getComputedStyle(el)
-      el.style.left = el.offsetLeft - parseFloat(style.marginLeft, 10) + 'px'
-      el.style.top = el.offsetTop - parseFloat(style.marginTop, 10) + 'px'
+Vue.component('my-component', {
+  template: '<div>{{ scrollY }}</div>',
+  computed: {
+    scrollY: function() {
+      return this.$window.scrollY
     }
   }
 })
